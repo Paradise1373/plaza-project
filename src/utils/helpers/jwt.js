59fb1b8f721993420d6ef8ab6c2130712f7d653a
Abdullahmaghsoudi <abdullahmaghsoudi@gmail.com>
2025-07-04 12:48:57 +0330

    added file helpers & jwt to my project

---
+++ src/utils/helpers/jwt.js
@@ -0,0 +1,21 @@
+import { SignJWT, jwtVerify } from 'jose'
+
+const key = new TextEncoder().encode('this is the secret key')
+
+export const encryptJWT = async (payload) => {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('1day')
+    .sign(key)
+}
+
+export const decryptJWT = async (session) => {
+  try {
+    const { payload } = await jwtVerify(session, key, (algorithm = ['HS256']))
+
+    return payload
+  } catch (error) {
+    console.log(null)
+  }
+}
